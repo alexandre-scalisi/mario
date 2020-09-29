6c7478a34bb2978d8bde418d7dc231e5b49f1dac
alexlemia13 <alexlemia13@outlook.fr>
2020-09-29 22:34:52 +0200

    .

--- exo_21.rb
+++ exo_21.rb
@@ -2,6 +2,8 @@ puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
 nombre = gets.chomp.to_i
 puts "Voici la pyramide :"
-nombre.times do |i|
+i = 0
+while i<nombre
     puts (" "*(nombre-(i+1)) + "#"*(i+1) )
+    i+=1
 end
