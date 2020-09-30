76f12adbff58dfdab20cc3cc2c85ad7b79c1b615
alexlemia13 <alexlemia13@outlook.fr>
2020-09-30 09:20:28 +0200

    .

--- exo_01.rb
+++ exo_01.rb
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 puts "Bonjour, monde !"
-=======
-puts "Bonjour monde !"
->>>>>>> 50f726180126ead8c96de8a40b2aca1a757875c9
--- exo_12.rb
+++ exo_12.rb
@@ -2,5 +2,5 @@ puts "Jusqu'a combien veux-tu compter?"
 nombre = gets.chomp.to_i
 
 nombre.times do |i|
-    puts i
+    puts (i+1)
 end
